c822a99b4d02ef6c921e5be4bd031edeb0389fc9
Bill Marcy <wgmarcy@gmail.com>
2021-04-27 00:46:23 -0400

    Add player controller and basic enemy movement

---
+++ Assets/Scripts/EnemyAnimation.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAnimation : MonoBehaviour
+{
+    
+    public float leftBoundary = 0.005f;
+    public float rightBoundary = -0.005f;
+
+    private float zRotationVelocity = -0.2f;
+    private float yRotationVelocity = 0.5f;
+
+    private float radius;
+
+    public Transform playerTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        radius = 0.5f * transform.lossyScale.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // "wobble"
+        if (transform.rotation.z <= rightBoundary ||
+            transform.rotation.z >= leftBoundary) {
+            zRotationVelocity *= -1;
+        }
+
+        transform.LookAt(playerTransform);
+        transform.position += Vector3.Normalize(transform.forward) * 0.01f;
+
+        // "walk" swivel 
+        // transform.parent.Rotate(0, yRotationVelocity, 0);
+
+        transform.Rotate(0, 0, zRotationVelocity);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 4;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dx = Input.GetAxisRaw("Horizontal");
+        float dz = Input.GetAxisRaw("Vertical");
+        Vector3 move = Vector3.Normalize(transform.right * dx + transform.forward * dz) * speed;
+        controller.Move(move * Time.deltaTime);
+    }
+}
