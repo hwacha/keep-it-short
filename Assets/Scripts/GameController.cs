ca9455894b0a54c776d6c3a2399e550ac5368878
Jack Kelly <jackckelly936@gmail.com>
2021-04-29 17:05:04 -0700

    adding UI elements and a game controller

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    /*
+        - references to the UI elements
+        - references to game state
+            we'll have some sort of game over state
+            we have the statuses of the NPCs
+                NPCs attributes
+                    - color
+                    - speed
+                    - wobble intensity? / some sort of angrier animation
+            we have the speech
+                it's broken up in some chunks
+                there are decision points with hardcoded options that will
+                trigger different dialogue
+
+                we
+
+
+    */
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+}
